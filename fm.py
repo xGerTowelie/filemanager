2bd445f73ffaddb22f73af691e2f0c240d082f38
towelie <mail@towelie.dev>
2024-08-26 15:00:07 +0200

    prototype of move and copy functionallity

--- fm.py
+++ fm.py
@@ -2,10 +2,13 @@ import os
 import urwid
 import subprocess
 import argparse
+import shutil
 
 # Define default paths
 DEFAULT_LEFT_PANE_PATH = "/home/towelie/"
 DEFAULT_RIGHT_PANE_PATH = "/home/towelie/"
+overwrite_confirmed = False
+copy_move_confirmed = False
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description="A simple file manager.")
@@ -20,6 +23,135 @@ RIGHT_PANE_PATH = DEFAULT_RIGHT_PANE_PATH
 FOLDER_SYMBOL = "📁"
 
 selected_items = set()
+
+def copy_items(source_path, dest_path):
+    for item in selected_items:
+        dest_item = os.path.join(dest_path, os.path.basename(item))
+        if os.path.exists(dest_item):
+            if not confirm_overwrite(dest_item):
+                continue
+        if os.path.isdir(item):
+            shutil.copytree(item, dest_item, dirs_exist_ok=True)
+        else:
+            shutil.copy2(item, dest_item)
+
+def move_items(source_path, dest_path):
+    for item in selected_items:
+        dest_item = os.path.join(dest_path, os.path.basename(item))
+        if os.path.exists(dest_item):
+            if not confirm_overwrite(dest_item):
+                continue
+        shutil.move(item, dest_item)
+
+def create_overwrite_dialog(item):
+    text = urwid.Text(f"'{os.path.basename(item)}' already exists. Overwrite?")
+    yes_button = urwid.Button("Yes", on_press=lambda _: on_overwrite_confirm(True))
+    no_button = urwid.Button("No", on_press=lambda _: on_overwrite_confirm(False))
+    
+    buttons = urwid.Columns([
+        ('weight', 1, urwid.AttrMap(yes_button, None, focus_map='reversed')),
+        ('weight', 1, urwid.AttrMap(no_button, None, focus_map='reversed')),
+    ])
+    
+    dialog_body = [text, urwid.Divider(), buttons]
+    dialog = NavigableDialog(dialog_body)
+    
+    return urwid.Overlay(
+        urwid.LineBox(dialog, title="Confirm Overwrite"),
+        columns,
+        align='center',
+        valign='middle',
+        width=('relative', 30),
+        height=('relative', 20),
+        min_width=20,
+        min_height=5
+    )
+
+def create_copy_move_dialog(operation):
+    text = urwid.Text(f"Are you sure you want to {operation} {len(selected_items)} item(s)?")
+    yes_button = urwid.Button("Yes", on_press=lambda _: on_copy_move_confirm(True, operation))
+    no_button = urwid.Button("No", on_press=lambda _: on_copy_move_confirm(False, operation))
+    
+    buttons = urwid.Columns([
+        ('weight', 1, urwid.AttrMap(yes_button, None, focus_map='reversed')),
+        ('weight', 1, urwid.AttrMap(no_button, None, focus_map='reversed')),
+    ])
+    
+    dialog_body = [text, urwid.Divider(), buttons]
+    dialog = NavigableDialog(dialog_body)
+    
+    return urwid.Overlay(
+        urwid.LineBox(dialog, title=f"Confirm {operation.capitalize()}"),
+        columns,
+        align='center',
+        valign='middle',
+        width=('relative', 30),
+        height=('relative', 20),
+        min_width=20,
+        min_height=5
+    )
+
+def on_copy_move_confirm(confirmed, operation):
+    global copy_move_confirmed, selected_items
+    copy_move_confirmed = confirmed
+    main_loop.widget = columns  # Close the dialog
+
+    if confirmed:
+        source_path = LEFT_PANE_PATH if current_focus == 0 else RIGHT_PANE_PATH
+        dest_path = RIGHT_PANE_PATH if current_focus == 0 else LEFT_PANE_PATH
+        
+        if operation == "copy":
+            copy_items(source_path, dest_path)
+        elif operation == "move":
+            move_items(source_path, dest_path)
+        
+        update_directory(0, LEFT_PANE_PATH)
+        update_directory(1, RIGHT_PANE_PATH)
+        selected_items.clear()
+
+def create_overwrite_dialog(item):
+    text = urwid.Text(f"'{os.path.basename(item)}' already exists. Overwrite?")
+    yes_button = urwid.Button("Yes", on_press=lambda _: on_overwrite_confirm(True))
+    no_button = urwid.Button("No", on_press=lambda _: on_overwrite_confirm(False))
+    
+    buttons = urwid.Columns([
+        ('weight', 1, urwid.AttrMap(yes_button, None, focus_map='reversed')),
+        ('weight', 1, urwid.AttrMap(no_button, None, focus_map='reversed')),
+    ])
+    
+    dialog_body = [text, urwid.Divider(), buttons]
+    dialog = NavigableDialog(dialog_body)
+    
+    return urwid.Overlay(
+        urwid.LineBox(dialog, title="Confirm Overwrite"),
+        columns,
+        align='center',
+        valign='middle',
+        width=('relative', 30),
+        height=('relative', 20),
+        min_width=20,
+        min_height=5
+    )
+
+def confirm_overwrite(item):
+    main_loop.widget = create_overwrite_dialog(item)
+    main_loop.run()
+    return overwrite_confirmed
+
+def on_overwrite_confirm(confirmed):
+    global overwrite_confirmed
+    overwrite_confirmed = confirmed
+    raise urwid.ExitMainLoop()
+
+def confirm_overwrite(item):
+    main_loop.widget = create_overwrite_dialog(item)
+    main_loop.run()
+    return overwrite_confirmed
+
+def on_overwrite_confirm(confirmed):
+    global overwrite_confirmed
+    overwrite_confirmed = confirmed
+    raise urwid.ExitMainLoop()
 
 def create_text_widget(text, path, focus=False):
     if path in selected_items:
@@ -238,17 +370,18 @@ class NavigableDialog(urwid.WidgetWrap):
 
     def keypress(self, size, key):
         if key in ('j', 'down'):
-            self.listbox.keypress(size, 'down')
+            return self.listbox.keypress(size, 'down')
         elif key in ('k', 'up'):
-            self.listbox.keypress(size, 'up')
+            return self.listbox.keypress(size, 'up')
         elif key == 'enter':
-            focus_widget, focus_position = self.listbox.body.get_focus()
-            if focus_widget:
-                focus_widget.base_widget._emit('click')
+            focus_widget, _ = self.listbox.body.get_focus()
+            if isinstance(focus_widget, urwid.AttrMap) and isinstance(focus_widget.original_widget, urwid.Button):
+                focus_widget.original_widget._emit('click')
+                return None
         elif key == 'esc':
             self.cancel_action()
-        else:
-            return super().keypress(size, key)
+            return None
+        return key
 
     def cancel_action(self):
         main_loop.widget = columns  # Close the dialog
@@ -306,22 +439,38 @@ def on_rename_confirm(new_name):
 
 def handle_input(key):
     global current_focus
+
     if isinstance(main_loop.widget, urwid.Overlay):
-        main_loop.widget.keypress((30, 20), key)
+        return main_loop.widget.keypress((30, 20), key)
+
+    elif key == 'c':
+        if selected_items:
+            main_loop.widget = create_copy_move_dialog("copy")
+
+    elif key == 'm':
+        if selected_items:
+            main_loop.widget = create_copy_move_dialog("move")
+
     elif key in ('j', 'down'):
         update_focus(current_focus, 1)
+
     elif key in ('k', 'up'):
         update_focus(current_focus, -1)
+
     elif key == 'd':
         main_loop.widget = create_delete_dialog()
+
     elif key == 'o':
         main_loop.widget = create_action_dialog()
+
     elif key == 'l':
         current_focus = 1
         columns.set_focus(1)
+
     elif key == 'h':
         current_focus = 0
         columns.set_focus(0)
+
     elif key == 'enter':
         listbox = left_listbox if current_focus == 0 else right_listbox
         current_path = LEFT_PANE_PATH if current_focus == 0 else RIGHT_PANE_PATH
@@ -331,19 +480,24 @@ def handle_input(key):
             new_path = os.path.join(current_path, item_name)
             if os.path.isdir(new_path):
                 update_directory(current_focus, new_path)
+
     elif key == 'backspace':
         current_path = LEFT_PANE_PATH if current_focus == 0 else RIGHT_PANE_PATH
         parent_path = os.path.dirname(current_path)
         if parent_path != current_path:
             update_directory(current_focus, parent_path)
+
     elif key == ' ':
         toggle_selection(current_focus)
+
     elif key == 'a':
         main_loop.widget = urwid.Overlay(AddDialog(), columns, align='center', valign='middle', width=('relative', 40), height=('relative', 20))
+
     elif key == 'r':
         focus_widget, _ = (left_listbox if current_focus == 0 else right_listbox).body.get_focus()
         old_name = focus_widget.base_widget.text.split(' ', 1)[-1].strip()
         main_loop.widget = urwid.Overlay(RenameDialog(old_name), columns, align='center', valign='middle', width=('relative', 40), height=('relative', 20))
+
     elif key in ('q', 'Q'):
         raise urwid.ExitMainLoop()
 
